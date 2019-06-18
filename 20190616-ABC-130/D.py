bbe394fc2872d047f2d6ddfb406ff6902fad8f76
uu64 <yusuke.t.0808@gmail.com>
2019-06-19 01:11:31 +0900

    ABC130 Dを尺取法で実装しなおした

--- 20190616-ABC-130/D.py
+++ 20190616-ABC-130/D.py
@@ -1,44 +1,29 @@
 # -*- coding: utf-8 -*-
-import math
-
-def bin_search(num_list, threshold):
-    len_half = int(len(num_list)/2)
-    idx = len_half
-    while True:
-        if idx < 0:
-            idx = 0
-        if idx == len(num_list):
-            idx = len(num_list) - 1
-
-        if num_list[idx] >= threshold:
-            if num_list[idx-1] < threshold:
-                return idx
-            else:
-                len_half = math.ceil(len_half/2)
-                offset = -len_half
-        else:
-            len_half = math.ceil(len_half/2)
-            offset = len_half
-
-        if idx == 0 or idx == len(num_list) - 1:
-            return len(num_list)
-        else:
-            idx += offset
 
 def main():
     N, K = map(int, input().split())
     a = list(map(int, input().split()))
-    a_sum = [a[0]]
-    for i in range(1, N):
-        a_sum.append(a_sum[i-1] + a[i])
 
     count = 0
-    for i in range(N):
-        if i == 0:
-            idx = bin_search(a_sum, K)
+    l_idx = 0
+    r_idx = 0
+    ruiseki_sum = 0
+    while True:
+        if ruiseki_sum < K:
+            if r_idx < len(a):
+                ruiseki_sum += a[r_idx]
+                r_idx += 1
+            else:
+                break
         else:
-            idx = bin_search(a_sum, K + a_sum[i-1])
-        count += N - idx
+            ruiseki_sum -= a[l_idx]
+            l_idx += 1
+
+        if ruiseki_sum >= K:
+            count += N - r_idx + 1
+
+        if r_idx == len(a) and l_idx == len(a):
+            break
 
     print(count)
 
