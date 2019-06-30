ecc730b5ad28abe8bba4d5ea01934c779a497c41
uu64 <yusuke.t.0808@gmail.com>
2019-07-01 01:11:48 +0900

    DをACに修正

--- 20190629-ABC-132/D.py
+++ 20190629-ABC-132/D.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
+def conbination(n, r):
+    return f[n] // (f[r] * f[n-r])
+
 N, K = map(int, input().split())
+
+f = [1]
+for i in range(1, N+1):
+    f.append(int(f[i-1]*i))
+
+for i in range(1, K+1):
+    if i > N-K+1:
+        print(0)
+        continue
+    c_blue = conbination(K-1, i-1)
+    c_red = conbination(N-K+1, i)
+    print((c_blue * c_red) % (10**9 + 7))
